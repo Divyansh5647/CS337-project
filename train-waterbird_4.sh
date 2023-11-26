955f6c6f3e011e7dd0326b99a955adda9619af29
AshwinG-14159 <96425398+AshwinG-14159@users.noreply.github.com>
2023-11-27 01:02:58 +0530

    Delete train-waterbird_4.sh

--- train-waterbird_4.sh
+++
@@ -1,9 +0,0 @@
-# CUDA_VISIBLE_DEVICES=0 python3 ./src/main.py --ensemble_size 1 --batch_size_train 128 --batch_size_eval 512 --seed 0 --epochs 300 --lr 0.001 --opt sgd --scheduler none --alpha 0 --perturb_type ood_is_test --model resnet18 --dataset waterbird --eval_freq 1000 --l2_reg 0.0001 --no_diversity
-CUDA_VISIBLE_DEVICES=2 python3 ./src/main.py --ensemble_size 4 --batch_size_train 128 --batch_size_eval 512 --seed 1 --epochs 300 --lr 0.001 --opt sgd --scheduler none --alpha 0 --perturb_type ood_is_test --model resnet18 --dataset waterbird --eval_freq 1000 --l2_reg 0.0001 --no_diversity
-CUDA_VISIBLE_DEVICES=2 python3 ./src/main.py --ensemble_size 4 --batch_size_train 128 --batch_size_eval 512 --seed 2 --epochs 300 --lr 0.001 --opt sgd --scheduler none --alpha 0 --perturb_type ood_is_test --model resnet18 --dataset waterbird --eval_freq 1000 --l2_reg 0.0001 --no_diversity
-
-
-# DBAT: D_ood = D_test
-# CUDA_VISIBLE_DEVICES=0 python3 ./src/main.py --ensemble_size 1 --batch_size_train 128 --batch_size_eval 512 --seed 0 --epochs 300 --lr 0.001 --opt sgd --scheduler none --alpha 0.0001 --perturb_type ood_is_test --model resnet18 --dataset waterbird --eval_freq 1000 --l2_reg 0.0001
-CUDA_VISIBLE_DEVICES=2 python3 ./src/main.py --ensemble_size 4 --batch_size_train 128 --batch_size_eval 512 --seed 1 --epochs 300 --lr 0.001 --opt sgd --scheduler none --alpha 0.0001 --perturb_type ood_is_test --model resnet18 --dataset waterbird --eval_freq 1000 --l2_reg 0.0001
-CUDA_VISIBLE_DEVICES=2 python3 ./src/main.py --ensemble_size 4 --batch_size_train 128 --batch_size_eval 512 --seed 2 --epochs 300 --lr 0.001 --opt sgd --scheduler none --alpha 0.0001 --perturb_type ood_is_test --model resnet18 --dataset waterbird --eval_freq 1000 --l2_reg 0.0001
